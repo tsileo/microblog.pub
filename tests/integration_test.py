525ba4e284a251bb8fd074227a13c4a3478b78dc
Thomas Sileo <t@a4.io>
2018-05-20 22:55:52 +0200

    Fix the test suite

--- tests/integration_test.py
+++ tests/integration_test.py
@@ -4,4 +4,4 @@ def test_ping_homepage():
     """Ensure the homepage is accessible."""
     resp = requests.get('http://localhost:5005')
     resp.raise_for_status()
-    assert 'ci@localhost' in resp.text
+    assert resp.status_code == 200
