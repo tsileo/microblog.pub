20a40ee34482357a10aa26bed67c48640bf860b1
Thomas Sileo <t@a4.io>
2018-07-07 14:07:53 +0200

    Tweak the CI delay

--- tests/federation_test.py
+++ tests/federation_test.py
@@ -20,7 +20,7 @@ class Instance(object):
     def __init__(self, name, host_url, docker_url=None):
         self.host_url = host_url
         self.docker_url = docker_url or host_url
-        self._create_delay = 15
+        self._create_delay = 12
         with open(
             os.path.join(
                 os.path.dirname(os.path.abspath(__file__)),
