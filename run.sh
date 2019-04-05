b7681b3a02e62e0e5da78c5b3b3473d45b58fcd9
Thomas Sileo <t@a4.io>
2019-04-05 15:14:57 +0200

    Fix CI

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python -c "import config; config.create_indexes()"
-gunicorn -t 300 -w 2 -b 0.0.0.0:5005 --log-level debug app:app
+gunicorn -t 300 -w 5 -b 0.0.0.0:5005 --log-level debug app:app
