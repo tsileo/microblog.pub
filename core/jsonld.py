6d1b342af17fe6da66a61f5126e0bab90baef7b6
Thomas Sileo <t@a4.io>
2019-08-17 21:02:43 +0200

    Add missing file

---
+++ core/jsonld.py
@@ -0,0 +1,17 @@
+MICROBLOGPUB = {
+    "@context": [
+        "https://www.w3.org/ns/activitystreams",
+        "https://w3id.org/security/v1",
+        {
+            "Hashtag": "as:Hashtag",
+            "PropertyValue": "schema:PropertyValue",
+            "manuallyApprovesFollowers": "as:manuallyApprovesFollowers",
+            "ostatus": "http://ostatus.org#",
+            "schema": "http://schema.org",
+            "sensitive": "as:sensitive",
+            "toot": "http://joinmastodon.org/ns#",
+            "totalItems": "as:totalItems",
+            "value": "schema:value",
+        },
+    ]
+}
