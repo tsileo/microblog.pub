5df4d420debf586da0bf9119b2810461bed087ed
João Costa <jdpc557@gmail.com>
2022-11-29 23:08:43 +0100

    Whitelist object types in the index query
    Select the outbox object types that we want to show on the notes page
    instead of removing objects that we don't want to show.
    That way, it's easier to ensure that there are no objects messing up the
    object count/empty checks.
    
    Partially fixes https://todo.sr.ht/~tsileo/microblog.pub/65

--- app/main.py
+++ app/main.py
@@ -296,7 +296,7 @@ async def index(
         models.OutboxObject.visibility == ap.VisibilityEnum.PUBLIC,
         models.OutboxObject.is_deleted.is_(False),
         models.OutboxObject.is_hidden_from_homepage.is_(False),
-        models.OutboxObject.ap_type != "Article",
+        models.OutboxObject.ap_type.in_(["Announce", "Note", "Video", "Question"]),
     )
     q = select(models.OutboxObject).where(*where)
     total_count = await db_session.scalar(
